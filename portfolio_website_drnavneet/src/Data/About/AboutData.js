6d59f97abcc6061e97fc5cdbfda9fcde7017068d
Raja Abrar Khan <abrarkhan778008@gmail.com>
2025-02-19 22:38:58 +0530

    commit

--- portfolio_website_drnavneet/src/Data/About/AboutData.js
+++ portfolio_website_drnavneet/src/Data/About/AboutData.js
@@ -1,15 +1,18 @@
 const AboutData={
-    para1:"Navnit Kumar Shukla is an accomplished academician, researcher, and consultant specializing in Marketing and Human Resource Management. With over eight years of experience, he serves as an Assistant Professor at the Uttaranchal Institute of Management, Uttaranchal University, Dehradun. His expertise spans sustainable marketing, entrepreneurship, and policy implementation. As a former Research Associate at IIM Lucknow and faculty at MJP Rohilkhand University, he has contributed to impactful studies in business strategy and innovation. Navnit has authored two books, published research papers in reputed journals, and is currently working on a textbook with Taylor & Francis. His academic contributions extend to book chapters, patents, and digital learning content.",
+    para1:"Navnit Kumar Shukla is an accomplished academician, researcher, and consultant specializing in Marketing , Human Resource Management and Entrepreneurship. With over eight years of experience, he serves as an Assistant Professor at the Uttaranchal Institute of Management, Uttaranchal University, Dehradun. His expertise spans sustainable marketing, entrepreneurship, and policy implementation. As a former Research Associate at IIM Lucknow and faculty at MJP Rohilkhand University, he has contributed to impactful studies in business strategy and innovation. Navnit has authored two books, published research papers in reputed journals, and is currently working on a textbook with Taylor & Francis. His academic contributions extend to book chapters, patents, and digital learning content.",
     para2:"Beyond academia, Navnit mentors startups as an Authorized State Mentor for Uttar Pradesh and Uttarakhand's Startup Councils, guiding 13 startups in business strategy. Recognized as a National Mentor for Startup India, he has shaped numerous ventures. As a keynote speaker, he has addressed topics like G20, Indian trade, and youth development on platforms including Doordarshan and the Ministry of Youth Affairs & Sports. Passionate about e-learning, he has developed over 30 digital learning resources. His dedication to education and innovation has earned him accolades, including the Shan-E-City Bareilly Award.",
     year1:"2011-2013",
+    year0:"2023",
+    edu0:"Post Graduate Diploma",
+    col0:"International Marketing and E Business (UPRT Open University)",
     edu1:"BA in Political Science and Hindi",
     col1:"D D U GORAKHPUR UNIVERSITY",
     year2:"2013-2015",
-    edu2:"MBA in Marketing and Human Resource",
+    edu2:"MBA in Marketing and Human Resource Management",
     col2:"MJP ROHILKHAND UNIVERSITY BAREILLY",
     year3:"2020-2024",
-    edu3:" PH.D",
-    col3:" Analytical Study Of Pm Svanidhi Scheme Launched During Covid Period",
+    edu3:" Ph.D.",
+    col3:"Study on a COVID-era public policy financial scheme.",
     expy1:"2024-Present",
     expd1:"ASSISTANT PROFESSOR",
     expp1:"Uttaranchal Institute of Management",
@@ -31,11 +34,10 @@ const AboutData={
 export default AboutData;
  
 export const affiliations = [
-    { title: "Mentor", description: "RIF Bareilly, IFTM & TMU (Startup Incubator)" },
+    { title: "Mentor", description: "RIF Bareilly" },
     { title: "National Mentor", description: "Startup India" },
     { title: "State Mentor", description: "Startup Council Uttar Pradesh" },
     { title: "Virtual State Mentor", description: "Startup Council Uttarakhand" },
-    { title: "Director", description: "Anveshan Translation Private Limited" },
     { title: "Marketing Consultant", description: "Unizal Pharmaceutical Pvt Limited" },
     { title: "Senior Vice President", description: "Shri Anandam Seva Trust (Under 80 G)" },
     { title: "Head of Policy Cell", description: "Youth Power Association (Reg NGO & 80 G)" },
@@ -47,6 +49,6 @@ export const affiliations = [
     { title: "City Ka Yaar 2018 Award", description: "By Radio City F.M" },
     { title: "Youth Icon Of Bareilly 2019", description: "" },
     { title: "Extraordinary Talent Award 2019", description: "For Educational and Social Work by Geniuses World Records" },
-    { title: "Corona Warrior", description: "By Pad Bank Foundation" },
+    { title: "Corona Warrior", description: "By Pad Bank Foundation Bareilly" },
   ];
   
--- portfolio_website_drnavneet/src/components/About/About.jsx
+++ portfolio_website_drnavneet/src/components/About/About.jsx
@@ -54,6 +54,11 @@ const About = () => {
                 <p className="font-normal text-lg sm:text-xl pt-1">{AboutData.col2}</p>
               </div>
               <div className="py-4">
+                <p className="text-2xl sm:text-3xl font-sm">{AboutData.year0}</p>
+                <p className="font-medium text-xl sm:text-2xl pt-1">{AboutData.edu0}</p>
+                <p className="font-normal text-lg sm:text-xl pt-1">{AboutData.col0}</p>
+              </div>
+              <div className="py-4">
                 <p className="text-2xl sm:text-3xl font-sm">{AboutData.year3}</p>
                 <p className="font-medium text-xl sm:text-2xl pt-1">{AboutData.edu3}</p>
                 <p className="font-normal text-lg sm:text-xl pt-1">{AboutData.col3}</p>
@@ -69,7 +74,7 @@ const About = () => {
               <div className="py-6">
                 <p className="text-2xl sm:text-3xl font-sm">{AboutData.expy1}</p>
                 <p className="font-medium text-base sm:text-xl pt-1">{AboutData.expd1}</p>
-                <p className="font-normal text-base sm:text-lg pt-1">{AboutData.expp2}</p>
+                <p className="font-normal text-base sm:text-lg pt-1">{AboutData.expp11}</p>
 
                 <p className="pt-4 text-2xl sm:text-3xl font-sm">{AboutData.expy2}</p>
                 <p className="font-medium text-base sm:text-xl pt-1">{AboutData.expd2}</p>
